3721e381647eddd7e4fe2630b517be1a8ea762e5
Richard Bagshaw <me@richardbagshaw.co.uk>
2021-03-25 19:43:28 +0000

    clean up tests

--- tests/CheckoutTest.php
+++ tests/CheckoutTest.php
@@ -16,14 +16,14 @@ class CheckoutTest extends TestCase
         $this->assertEquals(50, $checkout->total(), 'Checkout total does not equal expected value of 50');
     }
 
-    public function test_an_empty_checkout_returns_a_total_of_0()
+    public function test_an_empty_checkout_returns_a_total_of_zero()
     {
         $checkout = new Checkout();
 
         $this->assertEquals(0, $checkout->total(), 'Checkout total does not equal expected value of 0');
     }
 
-    public function test_adding_an_invalid_sku_doesnt_do_anything()
+    public function test_adding_an_invalid_sku_does_not_crash_the_checkout()
     {
         $checkout = new Checkout();
 
@@ -50,7 +50,22 @@ class CheckoutTest extends TestCase
         );
     }
 
-    public function test_the_discounted_price_is_used_when_ordering_three_of_A()
+    /**
+     * Provide various baskets full of items along with what the total of these items should be
+     *
+     * @return array
+     */
+    public function basketProvider()
+    {
+        return [
+            [100, ['A', 'A']],
+            [130, ['A', 'A', 'A']],
+            [130, ['A', 'A', 'B']],
+            [85, ['A', 'C', 'D']],
+        ];
+    }
+
+    public function test_the_price_is_discounted_when_ordering_three_times_a()
     {
         $checkout = new Checkout();
 
@@ -61,7 +76,7 @@ class CheckoutTest extends TestCase
         $this->assertEquals(130, $checkout->total(), 'Checkout total does not equal expected value of 130');
     }
 
-    public function test_the_discounted_price_is_used_multiple_times_when_ordering_six_of_A()
+    public function test_the_price_is_discounted_multiple_times_when_ordering_six_times_a()
     {
         $checkout = new Checkout();
 
@@ -76,22 +91,24 @@ class CheckoutTest extends TestCase
         $this->assertEquals(260, $checkout->total(), 'Checkout total does not equal expected value of 260');
     }
 
-    public function test_the_discounted_price_is_used_multiple_times_when_ordering_seven_of_A()
+    public function test_the_price_is_discounted_multiple_times_when_ordering_seven_times_a()
     {
         $checkout = new Checkout();
 
         $checkout->scan('A');
         $checkout->scan('A');
         $checkout->scan('A');
+
         $checkout->scan('A');
         $checkout->scan('A');
         $checkout->scan('A');
+
         $checkout->scan('A');
 
         $this->assertEquals(310, $checkout->total(), 'Checkout total does not equal expected value of 310');
     }
 
-    public function test_the_discounted_price_is_used_when_ordering_two_of_B()
+    public function test_the_price_is_discounted_when_ordering_two_times_b()
     {
         $checkout = new Checkout();
 
@@ -99,20 +116,5 @@ class CheckoutTest extends TestCase
         $checkout->scan('B');
 
         $this->assertEquals(45, $checkout->total(), 'Checkout total does not equal expected value of 45');
-    }
-
-    /**
-     * Provide various baskets full of items along with what the total of these items should be
-     *
-     * @return array
-     */
-    public function basketProvider()
-    {
-        return [
-            [100, ['A', 'A']],
-            [130, ['A', 'A', 'A']],
-            [130, ['A', 'A', 'B']],
-            [85, ['A', 'C', 'D']],
-        ];
     }
 }
